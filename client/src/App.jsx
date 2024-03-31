06c0b737e696a2802e42ccc7944831c10052fd25
AHADBAVA <ahaddesgins@gmail.com>
2024-03-31 14:19:29 +0530

    3d commit | Router Added

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,15 @@
-import React from 'react'
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import Login from './Login';
 
 const App = () => {
   return (
-    <div className='bg-red-300 h-screen h-screen'>
-      
+    <div className='bg-red-300 h-screen'>
+        <Routes>
+          <Route path="/employe/login" element={<Login />} />
+        </Routes>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
